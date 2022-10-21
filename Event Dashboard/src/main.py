1f35bad320cb1b4404f9a976eae4332d1b5705be
JavierMtzO <javiermtzo9912@gmail.com>
2022-10-21 00:58:29 -0500

    Dockerfile added

---
+++ Event Dashboard/src/main.py
@@ -0,0 +1,7 @@
+def main():
+    message = Message("Hello Event Dashboard!")
+    print(message)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Twitter Reloaded/src/main.py
@@ -0,0 +1,7 @@
+def main():
+    message = Message("Hello Twitter Reloaded!")
+    print(message)
+
+
+if __name__ == '__main__':
+    main()
